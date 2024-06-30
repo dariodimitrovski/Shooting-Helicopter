bb4a872121dfd919ea1c48998a7d3f89b9e1776f
Jana <janastojkovska00@gmail.com>
2024-06-30 20:44:53 +0200

    Completed

--- Shooting Helicopter/Form1.Designer.cs
+++ Shooting Helicopter/Form1.Designer.cs
@@ -45,9 +45,11 @@
             // 
             // pillar1
             // 
-            this.pillar1.Location = new System.Drawing.Point(518, 0);
+            this.pillar1.Image = global::Shooting_Helicopter.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(691, 0);
+            this.pillar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pillar1.Name = "pillar1";
-            this.pillar1.Size = new System.Drawing.Size(59, 172);
+            this.pillar1.Size = new System.Drawing.Size(79, 212);
             this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pillar1.TabIndex = 0;
             this.pillar1.TabStop = false;
@@ -56,9 +58,11 @@
             // 
             // pillar2
             // 
-            this.pillar2.Location = new System.Drawing.Point(253, 240);
+            this.pillar2.Image = global::Shooting_Helicopter.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(337, 295);
+            this.pillar2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pillar2.Name = "pillar2";
-            this.pillar2.Size = new System.Drawing.Size(60, 211);
+            this.pillar2.Size = new System.Drawing.Size(80, 260);
             this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pillar2.TabIndex = 1;
             this.pillar2.TabStop = false;
@@ -66,11 +70,13 @@
             // 
             // helicopter
             // 
+            this.helicopter.Image = global::Shooting_Helicopter.Properties.Resources.Halicopter;
             this.helicopter.ImageLocation = "";
             this.helicopter.InitialImage = null;
-            this.helicopter.Location = new System.Drawing.Point(62, 104);
+            this.helicopter.Location = new System.Drawing.Point(83, 128);
+            this.helicopter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.helicopter.Name = "helicopter";
-            this.helicopter.Size = new System.Drawing.Size(100, 54);
+            this.helicopter.Size = new System.Drawing.Size(133, 66);
             this.helicopter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.helicopter.TabIndex = 2;
             this.helicopter.TabStop = false;
@@ -79,7 +85,9 @@
             // 
             // ufo
             // 
-            this.ufo.Location = new System.Drawing.Point(705, 198);
+            this.ufo.Image = global::Shooting_Helicopter.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(940, 244);
+            this.ufo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ufo.Name = "ufo";
             this.ufo.Size = new System.Drawing.Size(68, 72);
             this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -97,9 +105,10 @@
             this.txtScore.AutoSize = true;
             this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtScore.ForeColor = System.Drawing.Color.White;
-            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Location = new System.Drawing.Point(16, 11);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(88, 24);
+            this.txtScore.Size = new System.Drawing.Size(110, 29);
             this.txtScore.TabIndex = 4;
             this.txtScore.Text = "Score: 0";
             // 
@@ -115,15 +124,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.ufo);
             this.Controls.Add(this.helicopter);
             this.Controls.Add(this.pillar2);
             this.Controls.Add(this.pillar1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Helicopter Shooting Game";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
