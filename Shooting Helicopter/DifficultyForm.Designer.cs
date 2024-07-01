3a5e5cc484bb2747aa26944eac638d1238056068
Jana <janastojkovska00@gmail.com>
2024-07-01 18:04:49 +0200

    Added new features

---
+++ Shooting Helicopter/DifficultyForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Shooting_Helicopter
+{
+    partial class DifficultyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.easyRadioButton = new System.Windows.Forms.RadioButton();
+            this.mediumRadioButton = new System.Windows.Forms.RadioButton();
+            this.hardRadioButton = new System.Windows.Forms.RadioButton();
+            this.OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // easyRadioButton
+            // 
+            this.easyRadioButton.AutoSize = true;
+            this.easyRadioButton.Location = new System.Drawing.Point(167, 84);
+            this.easyRadioButton.Name = "easyRadioButton";
+            this.easyRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.easyRadioButton.TabIndex = 0;
+            this.easyRadioButton.TabStop = true;
+            this.easyRadioButton.Text = "easy";
+            this.easyRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // mediumRadioButton
+            // 
+            this.mediumRadioButton.AutoSize = true;
+            this.mediumRadioButton.Location = new System.Drawing.Point(169, 135);
+            this.mediumRadioButton.Name = "mediumRadioButton";
+            this.mediumRadioButton.Size = new System.Drawing.Size(76, 20);
+            this.mediumRadioButton.TabIndex = 1;
+            this.mediumRadioButton.TabStop = true;
+            this.mediumRadioButton.Text = "medium";
+            this.mediumRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // hardRadioButton
+            // 
+            this.hardRadioButton.AutoSize = true;
+            this.hardRadioButton.Location = new System.Drawing.Point(170, 184);
+            this.hardRadioButton.Name = "hardRadioButton";
+            this.hardRadioButton.Size = new System.Drawing.Size(55, 20);
+            this.hardRadioButton.TabIndex = 2;
+            this.hardRadioButton.TabStop = true;
+            this.hardRadioButton.Text = "hard";
+            this.hardRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(170, 239);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 3;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(100, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose the level";
+            // 
+            // DifficultyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(431, 301);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.hardRadioButton);
+            this.Controls.Add(this.mediumRadioButton);
+            this.Controls.Add(this.easyRadioButton);
+            this.Name = "DifficultyForm";
+            this.Text = "DifficultyForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton easyRadioButton;
+        private System.Windows.Forms.RadioButton mediumRadioButton;
+        private System.Windows.Forms.RadioButton hardRadioButton;
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Shooting Helicopter/DifficultyForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Shooting_Helicopter
+{
+    public partial class DifficultyForm : Form
+    {
+        public string SelectedDifficulty { get; private set; }
+
+        public DifficultyForm()
+        {
+            InitializeComponent();
+            this.FormClosing+= DifficultyForm_FormClosing;
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void DifficultyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.None)
+            {
+
+                SelectedDifficulty = "Easy";
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            if (easyRadioButton.Checked)
+            {
+                SelectedDifficulty = "Easy";
+            }
+            else if (mediumRadioButton.Checked)
+            {
+                SelectedDifficulty = "Medium";
+            }
+            else if (hardRadioButton.Checked)
+            {
+                SelectedDifficulty = "Hard";
+            }
+            else
+            {
+                SelectedDifficulty = "Default";
+            }
+
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
